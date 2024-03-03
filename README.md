849cc0a90e77aac220a4bb81c2df891305c25a78
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-03-03 09:19:13 -0500

    chore: regens README.md

--- README.md
+++ README.md
@@ -46,8 +46,7 @@
 
 - p6df::modules::bash::deps()
 - p6df::modules::bash::external::brew()
-- p6df::modules::bash::home::symlink()
-- p6df::modules::bash::init()
+
 
 
 ## Author
--- init.zsh
+++ init.zsh
@@ -6,7 +6,7 @@
 #
 #>
 ######################################################################
-p6df::modules::bash::deps()    { 
+p6df::modules::bash::deps() {
   ModuleDeps=(
     p6m7g8-dotfiles/p6common
   )
